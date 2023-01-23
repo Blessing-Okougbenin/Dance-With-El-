25ea33e56d3f387d79a614d19247e84258d56913
ElijahChinonso <okougbeninelijah003@gmail.com>
2023-01-23 05:02:54 +0100

    created the landing page.

---
+++ src/pages/home/components/landPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from "./button";
+
+export default function LandPage () {
+    return(
+        <div className={'landing_page'}>
+            <Button/>
+        </div>
+    )
+}
