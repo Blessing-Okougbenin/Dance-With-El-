327814b54db13f0e71cb8d7140fbbba9ec2b156b
ElijahChinonso <okougbeninelijah003@gmail.com>
2023-01-23 23:54:29 +0100

    created a title component.

---
+++ src/pages/landingPage/components/title.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/title.css'
+
+export default function Title () {
+    return(
+        <div className={'titleText_div'}>
+            <div className={'title_div'}>
+                <h1 className={'title'}>EL' DANCE CLASS</h1>
+            </div>
+
+            <div className={'div_a'}></div>
+
+            <div className={'div_b'}></div>
+        </div>
+    )
+}
