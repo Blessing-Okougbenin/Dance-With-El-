22fa9472d7bbde8eb3236cd998fa5b284ca6f3e7
ElijahChinonso <okougbeninelijah003@gmail.com>
2023-01-24 03:05:06 +0100

    edited the hero component.

--- src/pages/home/components/hero.js
+++ src/pages/home/components/hero.js
@@ -1,10 +1,56 @@
 import React from "react";
 import '../styles/hero.css'
+import BalletKids from '../../../asset/images/Ballet-kids.jpg'
+import SalsaBatchata from '../../../asset/images/Salsa-Bachata-Cha-Cha-Rumba.jpg'
+import DancingWoman from '../../../asset/images/dancing-woman-black-dress-contemporary.jpg'
+import WomenDancingContemporary from '../../../asset/images/Women-Dancing-Contemporary-On-Stage.jpg'
 
 export default function Hero () {
     return(
         <div className={"hero"}>
+            <div className={'line_one'}>
+                <div className={'ballet_kids_div'}>
+                    <img className={'ballet_kids_img'} src={BalletKids} alt={'Ballet_kids'}/>
 
+                    <p className={'ballet_kids_text'}>Have your kids do the best while they can, Such investment is of a lifetime.
+                        In our modern day today, several reasons our parent want their
+                        offspring to explore different art-motivated activities.
+                        The very first reason is for the good mental health of the child, it keeps them smart and effective at all time.
+                    </p>
+                </div>
+
+                <div className={'batchata_div'}>
+                    <img className={'batchata_img'} src={SalsaBatchata} alt={'salsa'}/>
+
+                    <p className={'batchata_text'}>Have your kids do the best while they can, Such investment is of a lifetime.
+                        In our modern day today, several reasons our parent want their
+                        offspring to explore different art-motivated activities.
+                        The very first reason is for the good mental health of the child, it keeps them smart and effective at all time.
+                    </p>
+                </div>
+            </div>
+
+            <div className={'line_two'}>
+                <div className={'dancingWoman_div'}>
+                    <img className={'dancingWoman_img'} src={DancingWoman} alt={'salsa'}/>
+
+                    <p className={'dancingWoman_text'}>Have your kids do the best while they can, Such investment is of a lifetime.
+                        In our modern day today, several reasons our parent want their
+                        offspring to explore different art-motivated activities.
+                        The very first reason is for the good mental health of the child, it keeps them smart and effective at all time.
+                    </p>
+                </div>
+
+                <div className={'WomenDancingContemporary_div'}>
+                    <img className={'WomenDancingContemporary_img'} src={WomenDancingContemporary} alt={'salsa'}/>
+
+                    <p className={'WomenDancingContemporary_text'}>Have your kids do the best while they can, Such investment is of a lifetime.
+                        In our modern day today, several reasons our parent want their
+                        offspring to explore different art-motivated activities.
+                        The very first reason is for the good mental health of the child, it keeps them smart and effective at all time.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
